78e959847de8cc8037b3fcd6b65cbc8f4651a602
Damitry Dong <Damitry111@gmail.com>
2025-02-28 22:37:11 -0600

    added the 3 componetsn on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
-import GeminiTest from "@/Componenets/Gemini";
+
 import MapBoxMap from "@/Componenets/MapBox";
+import ImageCards from "@/Componenets/postCards/ImageCards";
+import { LocationSearch } from "@/Componenets/LocationSearch";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center bg-white text-black">
-        <GeminiTest/>
-        <MapBoxMap/>
+    <div className="flex flex-row justify-center bg-white text-black">
+      <ImageCards/>
+      <MapBoxMap/>
+      <LocationSearch/> 
     </div>
   );
 }
